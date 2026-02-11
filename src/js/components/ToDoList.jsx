abb68b1c8d2814e87df370e7c980759ebb296d28
Ruben Alba Gonzalez <39350323+Ruben-Alba-Gonzalez@users.noreply.github.com>
2026-02-11 18:50:53 +0000

    Añadidas mejoras lógicas

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import React from "react";
+import { useState} from "react";
 
 
 const ToDoList = () => {
@@ -17,7 +16,7 @@ const ToDoList = () => {
 		}
 		setTareas([...tareas, nuevaTarea])
 		setNuevaTarea("");
-		
+
 	}
 
 	function borrarTarea(index) {
@@ -25,22 +24,24 @@ const ToDoList = () => {
 		setTareas(tareasActualizadas);
 	}
 
+	const taskList = tareas.map((task, taskIndex) =>
+		<li key={taskIndex} className="w-100">
+			<span>{task}</span>
+			<div className="d-flex justify-content-end px-2">
+				<button className="btn btn-danger" onClick={() => borrarTarea(taskIndex)}>Borrar</button>
+			</div>
+		</li>
+	)
+
 	return (
 		<div className="container mx-auto text-center">
 			<h1 className="title">To Do List</h1>
 			<div className="w-100 mx-auto d-flex justify-content-center">
-				<input className="w-50 mx-1" type="text" placeholder="Añade una tarea" value={nuevaTarea} onChange={cambioInput} onKeyDown={(e)=>anadirTarea(e)}/>
-				<button className="btn btn-success" onClick={(e)=>anadirTarea(e)}>Añadir</button>
+				<input className="w-50 mx-1" type="text" placeholder="Añade una tarea" value={nuevaTarea} onChange={cambioInput} onKeyDown={(e) => anadirTarea(e)} />
+				<button className="btn btn-success" onClick={(e) => anadirTarea(e)}>Añadir</button>
 			</div>
-			<ul>
-				{tareas.map((task, taskIndex) =>
-					<li key={taskIndex} className="">
-						<span>{task}</span>
-						<div>
-							<button className="btn btn-danger" onClick={() => borrarTarea(taskIndex)}>Borrar</button>
-						</div>
-					</li>
-				)}
+			<ul className="w-50 mx-auto p-0">
+				{tareas.length === 0 ? <span className="conditionalText fs-2">No hay tareas, añadir tareas</span> : taskList}
 			</ul>
 		</div>
 	);
